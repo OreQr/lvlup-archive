49c33dce68a325eb0770f0583b863d66da32bc1c
OreQr <50458706+OreQr@users.noreply.github.com>
2024-06-17 06:05:08 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,28 @@
+import "dotenv/config"
+
 import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
+import robotsTxt from "astro-robots-txt"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.APP_URL,
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
+    }),
+    sitemap({
+      filter: (page) => {
+        const segments = page.split("/").filter((e) => e !== "")
+
+        return segments.length === 5 || segments.length === 2
+      },
+    }),
+    robotsTxt({
+      policy: [{ allow: "/", userAgent: "*" }],
     }),
   ],
 })
