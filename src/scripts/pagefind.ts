3db87770147daa3c33eac1a78f937596725d1313
OreQr <50458706+OreQr@users.noreply.github.com>
2024-06-22 16:16:48 +0200

    Refactor pagefind and new search ui

--- src/scripts/pagefind.ts
+++ src/scripts/pagefind.ts
@@ -1,4 +1,5 @@
 import { Presets, SingleBar } from "cli-progress"
+import { close, createIndex } from "pagefind"
 
 import { postToRaw } from "@/lib/markdown"
 import { posts } from "@/lib/posts"
@@ -7,32 +8,31 @@ import { site } from "@/lib/site"
 const progress = new SingleBar({}, Presets.shades_classic)
 
 const indexContent = async () => {
-  const { createIndex, close } = await import("pagefind")
   const { index } = await createIndex({})
-  if (!index) return
 
   progress.start(posts.length, 0)
-  for (const post of posts) {
-    const content = await postToRaw(post)
 
-    const category = site.categories.find(
-      (category) => category.id === post.category_id
-    )!
+  await Promise.all(
+    posts.map(async (post) => {
+      const content = await postToRaw(post)
 
-    await index.addCustomRecord({
-      url: `/t/${post.slug}/${post.topic_id}`,
-      content,
-      meta: {
-        title: post.fancy_title ? post.fancy_title : post.title,
-        category: category.name,
-      },
-      filters: {
-        tags: post.tags,
-      },
-      language: "pl",
+      const category = site.categories.find(
+        (category) => category.id === post.category_id
+      )!
+
+      await index.addCustomRecord({
+        url: `/t/${post.slug}/${post.topic_id}`,
+        content,
+        meta: {
+          title: post.fancy_title ? post.fancy_title : post.title,
+          image: post.image || undefined,
+          kategoria: category.name,
+        },
+        language: "pl",
+      })
+      progress.increment()
     })
-    progress.increment()
-  }
+  )
 
   await index.writeFiles({
     outputPath: "public/pagefind",
